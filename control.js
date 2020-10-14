7fe8785a1c7464898355f67c4a8612edbf66bf2f
marinanikolaj2013 <marinanikolaj2013@gmail.com>
2020-10-14 15:07:24 -0300

    еще

--- control.js
+++ control.js
@@ -1,14 +1,18 @@
-
 var form = document.getElementById('form');
-var email = document.getElementById('email');
+var text = document.getElementById('text');
 var password = document.getElementById('password');
 var RePassword = document.getElementById('RePassword')
 var submit = document.getElementById('submit');
 var para = document.getElementById("error-descr");
 
 form.onsubmit = function (e) {
-    if (email.value == "" || password.value == "") {
+    if (!text.value.includes("@")) {
+        e.preventDefault();
+        para.textContent = 'Falta el arroba en el usuario!';
+    }
+    if (text.value == "" || password.value == "") {
         e.preventDefault();
         para.textContent = 'Completa ambos datos!';
     }
+
 }
